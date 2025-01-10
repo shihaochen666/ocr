b53ab2b04ec862654d80814dc53d86b1d254f6bb
h10086 <h10086733@163.com>
2025-01-10 23:14:40 +0800

    update:

--- core/analysis.py
+++ core/analysis.py
@@ -41,12 +41,13 @@ class Analysis:
         data = data.sort_values(by=['index_1', 'index_2'], ascending=[True, True])
         # 行高容错设置
         data["row_height"] = abs(data["index_4"] - data["index_6"]) / 2.5
-        data["y_offset_up"] = data["index_2"] + data["row_height"]
-        data["y_offset_low"] = data["index_2"] - data["row_height"]
-        data["x_offset_up"] = data["index_3"] * 1.01
-        data["x_offset_low"] = data["index_7"] * 0.99
+        data["filed_height"] = data["index_6"] - data["index_4"]
         data["filed_length"] = data["index_3"] - data["index_7"]
-        data["filed_height"] = (data["index_6"] - data["index_4"])
+
+        data["y_offset_up"] = data["index_2"] + data["filed_height"] * 0.5
+        data["y_offset_low"] = data["index_2"] - data["filed_height"] * 0.5
+        data["x_offset_up"] = data["index_3"] + data["filed_height"] * 0.5
+        data["x_offset_low"] = data["index_7"] - data["filed_height"] * 0.5
         self.middle_y = (max(data["index_2"]) - min(data["index_2"])) / 2
         self.middle_x = (max(data["index_3"]) - min(data["index_3"])) / 2
         print(data)
@@ -55,6 +56,7 @@ class Analysis:
     def data_handle(self, ocr_type: str):
         keys = self.data["key"].tolist()
         return getattr(self, ocr_type + "_analysis")()
+
 
     def merge_raw_data(self, fileds: dict):
         """
@@ -186,18 +188,20 @@ class Analysis:
         发票号码 = self.analysis_index(key="发票号码:", direction="like")
         开票人 = self.analysis_index(key="开票人:", direction="like")
 
-
-        项目名称 = self.analysis_index(key="项目名称", direction="below")
+        项目名称 = self.analysis_index(key="项目名称", direction="below", below_height=8)
         规格型号 = self.analysis_index(key="规格型号", direction="below")
         单位 = self.analysis_index(key="单位", direction="below")
         数量 = self.analysis_index(key="数量", direction="below")
         金额 = self.analysis_index(key="金额", direction="below")
         税额 = self.analysis_index(key="税额", direction="below")
-        金额合计 = self.analysis_index(key="金额",end_key="￥", direction="below")
+        金额合计 = self.analysis_index(key="金额", end_key="￥", direction="below")
+        税额合计 = self.analysis_index(key="税额", end_key="￥", direction="below")
+        if 金额合计:
+            金额合计=金额合计[0]
 
         data = {"开票日期": 开票日期, "发票号码": 发票号码, "购买方名称": 购买方名称, "销售方名称": 销售方名称,
                 "价税合计大写": 价税合计大写,"价税合计小写": 价税合计小写, "项目名称": 项目名称,
-                "规格型号": 规格型号, "单位": 单位, "数量": 数量, "金额": 金额,"金额合计":金额合计, "合计税额": 税额, "开票人": 开票人}
+                "规格型号": 规格型号, "单位": 单位, "数量": 数量, "金额": 金额,"税额":税额,"金额合计":金额合计, "税额合计": 税额合计, "开票人": 开票人}
         print(data)
         return data
 
@@ -206,9 +210,12 @@ class Analysis:
         data = {}
         print(data)
         return data
-   
+    
+    def clean_data(self,key,data_list):
+        # 使用列表推导式移除与 `key` 相等的项
+        return [item for item in data_list if item != key]
 
-    def analysis_index(self, key, direction, end_key=None, block=-1):
+    def analysis_index(self, key, direction, end_key=None, block=-1, below_height=2):
 
         """
         解析key 方向的匹配
@@ -216,31 +223,32 @@ class Analysis:
         :param direction: below / right like
         :return:
         """
+
         append_block_filter = ""
         if block == 1:
-            append_block_filter = f"and  index_2 >= {self.middle_y} and index_3 <= {self.middle_x}"
+            append_block_filter = f"and  index_2 >= {self.middle_y} and index_7 <= {self.middle_x}"
         if block == 2:
-            append_block_filter = f"and index_2 >= {self.middle_y} and index_3 >= {self.middle_x}"
+            append_block_filter = f"and index_2 >= {self.middle_y} and index_7 >= {self.middle_x}"
         if block == 3:
-            append_block_filter = f"and index_2 <= {self.middle_y} and  index_3 <= {self.middle_x}"
+            append_block_filter = f"and index_2 <= {self.middle_y} and  index_7 <= {self.middle_x}"
         if block == 4:
-            append_block_filter = f"and index_2 <= {self.middle_y} and index_3 >= {self.middle_x}"
+            append_block_filter = f"and index_2 <= {self.middle_y} and index_7 >= {self.middle_x}"
 
         start_in_words = self.data.query(f'key=="{key}"')
         if direction == "like":
             expr = 'key.str.contains(@key, case=False, na=False)'
             expr += append_block_filter
             curr_key = self.data.query(expr, engine='python')
-            return curr_key["key"].tolist()
+            return self.clean_data(key,curr_key["key"].tolist())
         if end_key is not None:
-            end_in_words = self.data.query('key.str.contains(@end_key, case=False, na=False)', engine='python')
+            end_key = end_key.strip()
+            end_in_words = self.data[self.data['key'].str.startswith(end_key, na=False)]
             end_row = end_in_words.iloc[0]
         if not start_in_words.empty:
             first_row = start_in_words.iloc[0]
-
             if end_key is not None and direction == "right":
-
-                query_str = f'{first_row["y_offset_low"]} < index_2 < {first_row["y_offset_up"]} and index_1 != {first_row["index_1"]} and index_7 <= {end_row["index_7"]}'
+                # query_str = f'{first_row["y_offset_low"]} <= index_2 <= {first_row["y_offset_up"]} and index_1 != {first_row["index_1"]} and index_7 <= {end_row["index_7"]}'
+                query_str = f'{first_row["y_offset_low"]} <= index_2 <= {first_row["y_offset_up"]} and index_1 != {first_row["index_1"]} and index_7 <= {end_row["index_3"]}'
                 query_str += append_block_filter
                 filter_values_words_value = self.data.query(query_str)
 
@@ -251,18 +259,18 @@ class Analysis:
 
             elif end_key is None and direction == "below":
                 # 左对齐 或右对齐
-                filed_height = first_row["filed_height"]
-                query_str = f' ({first_row["x_offset_low"]} < index_3 < {first_row["x_offset_up"]} and {first_row["index_2"]} <= index_2 <= {first_row["index_2"]}+ {filed_height * 2} and index_2 != {first_row["index_2"]}) or ({first_row["x_offset_low"]} < index_7 < {first_row["x_offset_up"]} and {first_row["index_2"]} <= index_2 <= {first_row["index_2"]}+ {filed_height * 2} and index_2 != {first_row["index_2"]})'
+                filed_height = first_row["filed_height"] * below_height
+                query_str = f' {first_row["x_offset_low"]} <= index_3 and {first_row["x_offset_up"]} >= index_7 and  {first_row["index_2"]}+{filed_height} >= index_2  and {first_row["index_2"]}<= index_2'
+                # query_str = f' ({first_row["x_offset_low"]} < index_3 < {first_row["x_offset_up"]} and {first_row["index_2"]} <= index_2 <= {first_row["index_2"]}+ {filed_height * 2} and index_2 != {first_row["index_2"]}) or ({first_row["x_offset_low"]} < index_7 < {first_row["x_offset_up"]} and {first_row["index_2"]} <= index_2 <= {first_row["index_2"]}+ {filed_height * 2} and index_2 != {first_row["index_2"]})'
                 query_str += append_block_filter
                 filter_values_words_value = self.data.query(query_str)
 
             elif end_key is not None and direction == "below":
                 # 左对齐 或右对齐
-                filed_height = end_row["filed_height"]
-                query_str = f' ({first_row["x_offset_low"]} < index_3 < {first_row["x_offset_up"]} and {first_row["index_2"]} <= index_2 <= {first_row["index_2"]}+ {filed_height * 2} and index_2 != {first_row["index_2"]}) or ({first_row["x_offset_low"]} < index_7 < {first_row["x_offset_up"]} and {first_row["index_2"]} <= index_2 <= {first_row["index_2"]}+ {filed_height * 2} and index_2 != {first_row["index_2"]})'
+                query_str = f' {first_row["x_offset_low"]} < index_3 and {first_row["x_offset_up"]} > index_7 and  {end_row["index_2"]} > y_offset_low  and {end_row["index_2"]} < y_offset_up'
                 query_str += append_block_filter
                 filter_values_words_value = self.data.query(query_str)
-            return filter_values_words_value["key"].tolist()
+            return self.clean_data(key,filter_values_words_value["key"].tolist())
         else:
             return []
 
